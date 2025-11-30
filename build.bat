10bda6d0ba207ca005e4d6ee80df450cb36acbfa
Raghavendra Reddy <ragahvendrareddy97013838@gmail.com>
2025-11-30 09:27:17 +0530

    fix the CI issues

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 set MSYS2_SHELL=C:\msys64\msys2_shell.cmd
-set SCRIPT=/d/Application/client/build_client.sh
+set SCRIPT=/d/Application/build_client.sh
 
 "%MSYS2_SHELL%" -mingw64 -full-path -here -c "bash %SCRIPT%"
 
